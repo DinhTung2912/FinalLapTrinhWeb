1be4bc470f5d892c856efb3c93e22d870598e261
DinhTung2912 <19130259@st.hcmuaf.edu.vn>
2023-12-13 15:17:03 +0700

    update coe

--- src/main/java/com/example/finallaptrinhweb/model/Order.java
+++ src/main/java/com/example/finallaptrinhweb/model/Order.java
@@ -1,108 +1,211 @@
 
 package com.example.finallaptrinhweb.model;
 
-import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 public class Order {
     private int id;
-    private String orderCode;
+    private String username;
     private int userId;
-    private int productId;
+    private int discountsId;
+    private int shipId;
     private int quantity;
-    private Timestamp orderDate;
     private String status;
-    private int orderDiscountId;
-    private BigDecimal totalAmount;
+    private double totalAmount;
+    private long phone;
+    private String detailAddress;
+    private boolean payment;
+    private Timestamp dateCreated;
+    private double totalPay;
+    private int countId;
+    private double shipPrice;
+    private String nameProduct;
+    private int numberProduct;
+
 
     public Order() {
+
     }
 
-    public Order(int id, String orderCode, int userId, int productId, int quantity, Timestamp orderDate, String status, int orderDiscountId, BigDecimal totalAmount) {
+    public Order(int id, String username, int userId, int discountsId, int shipId, int quantity, String status,
+                 double totalAmount, long phone, String detailAddress, boolean payment, Timestamp dateCreated,
+                 double totalPay, int countId, double shipPrice, String nameProduct, int numberProduct) {
         this.id = id;
-        this.orderCode = orderCode;
+        this.username = username;
         this.userId = userId;
-        this.productId = productId;
+        this.discountsId = discountsId;
+        this.shipId = shipId;
         this.quantity = quantity;
-        this.orderDate = orderDate;
         this.status = status;
-        this.orderDiscountId = orderDiscountId;
         this.totalAmount = totalAmount;
+        this.phone = phone;
+        this.detailAddress = detailAddress;
+        this.payment = payment;
+        this.dateCreated = dateCreated;
+        this.totalPay = totalPay;
+        this.countId = countId;
+        this.shipPrice = shipPrice;
+        this.nameProduct = nameProduct;
+        this.numberProduct = numberProduct;
     }
 
     public int getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getOrderCode() {
-        return this.orderCode;
+    public String getUsername() {
+        return username;
     }
 
-    public void setOrderCode(String orderCode) {
-        this.orderCode = orderCode;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public int getUserId() {
-        return this.userId;
+        return userId;
     }
 
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
-    public int getProductId() {
-        return this.productId;
+    public int getDiscountsId() {
+        return discountsId;
     }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
+    public void setDiscountsId(int discountsId) {
+        this.discountsId = discountsId;
+    }
+
+    public int getShipId() {
+        return shipId;
+    }
+
+    public void setShipId(int shipId) {
+        this.shipId = shipId;
     }
 
     public int getQuantity() {
-        return this.quantity;
+        return quantity;
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 
-    public Timestamp getOrderDate() {
-        return this.orderDate;
-    }
-
-    public void setOrderDate(Timestamp orderDate) {
-        this.orderDate = orderDate;
-    }
-
     public String getStatus() {
-        return this.status;
+        return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
     }
 
-    public int getOrderDiscountId() {
-        return this.orderDiscountId;
+    public double getTotalAmount() {
+        return totalAmount;
     }
 
-    public void setOrderDiscountId(int orderDiscountId) {
-        this.orderDiscountId = orderDiscountId;
-    }
-
-    public BigDecimal getTotalAmount() {
-        return this.totalAmount;
-    }
-
-    public void setTotalAmount(BigDecimal totalAmount) {
+    public void setTotalAmount(double totalAmount) {
         this.totalAmount = totalAmount;
     }
 
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getDetailAddress() {
+        return detailAddress;
+    }
+
+    public void setDetailAddress(String detailAddress) {
+        this.detailAddress = detailAddress;
+    }
+
+    public boolean isPayment() {
+        return payment;
+    }
+
+    public void setPayment(boolean payment) {
+        this.payment = payment;
+    }
+
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public double getTotalPay() {
+        return totalPay;
+    }
+
+    public void setTotalPay(double totalPay) {
+        this.totalPay = totalPay;
+    }
+
+    public int getCountId() {
+        return countId;
+    }
+
+    public void setCountId(int countId) {
+        this.countId = countId;
+    }
+
+    public double getShipPrice() {
+        return shipPrice;
+    }
+
+    public void setShipPrice(double shipPrice) {
+        this.shipPrice = shipPrice;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public int getNumberProduct() {
+        return numberProduct;
+    }
+
+    public void setNumberProduct(int numberProduct) {
+        this.numberProduct = numberProduct;
+    }
+
+    @Override
     public String toString() {
-        return "Order{id=" + this.id + ", orderCode='" + this.orderCode + "', userId=" + this.userId + ", productId=" + this.productId + ", quantity=" + this.quantity + ", orderDate=" + this.orderDate + ", status='" + this.status + "', orderDiscountId=" + this.orderDiscountId + ", totalAmount=" + this.totalAmount + "}";
+        return "Order{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", userId=" + userId +
+                ", discountsId=" + discountsId +
+                ", shipId=" + shipId +
+                ", quantity=" + quantity +
+                ", status='" + status + '\'' +
+                ", totalAmount=" + totalAmount +
+                ", phone=" + phone +
+                ", detailAddress='" + detailAddress + '\'' +
+                ", payment=" + payment +
+                ", dateCreated=" + dateCreated +
+                ", totalPay=" + totalPay +
+                ", countId=" + countId +
+                ", shipPrice=" + shipPrice +
+                ", nameProduct='" + nameProduct + '\'' +
+                ", numberProduct=" + numberProduct +
+                '}';
     }
 }
+
