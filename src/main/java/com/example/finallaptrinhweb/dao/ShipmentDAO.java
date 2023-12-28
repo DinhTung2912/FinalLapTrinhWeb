d9e4083ebac6e4f8b3ca9bff3428a26d07052e1f
DinhTung2912 <19130259@st.hcmuaf.edu.vn>
2023-12-28 18:43:15 +0700

    up code order

--- src/main/java/com/example/finallaptrinhweb/dao/ShipmentDAO.java
+++ src/main/java/com/example/finallaptrinhweb/dao/ShipmentDAO.java
@@ -1,6 +1,5 @@
 package com.example.finallaptrinhweb.dao;
 
-
 import com.example.finallaptrinhweb.connection_pool.DBCPDataSource;
 import com.example.finallaptrinhweb.model.ShippingInfo;
 
@@ -9,6 +8,88 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.math.BigDecimal;
 
 public class ShipmentDAO {
+
+    public static List<ShippingInfo> getAllShipments() {
+        List<ShippingInfo> shipmentList = new ArrayList<>();
+        try {
+            String sql = "SELECT id, shippingCost, date_created, weight FROM shipping_info";
+            PreparedStatement preparedStatement = DBCPDataSource.preparedStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                ShippingInfo shipment = new ShippingInfo();
+                shipment.setId(resultSet.getInt("id"));
+                shipment.setShippingCost(resultSet.getBigDecimal("shippingCost"));
+                shipment.setDateCreated(resultSet.getDate("date_created"));
+                shipment.setWeight(resultSet.getDouble("weight"));
+                shipmentList.add(shipment);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return shipmentList;
+    }
+
+    public static int addShipment(int typeWeight) {
+        int id = getNextId();
+        int updated = 0;
+        double price = 0;
+
+        // Sử dụng if-else thay thế cho switch với typeWeight
+        if (typeWeight == 1) {
+            price = 10000;
+        } else if (typeWeight == 2) {
+            price = 20000;
+        } else if (typeWeight == 3) {
+            price = 50000;
+        } else if (typeWeight == 4) {
+            price = 70000;
+            String sql = "INSERT INTO shipping_info (shippingCost, date_created, weight, id) VALUES (?, ?, ?, ?)";
+            try {
+                PreparedStatement preparedStatement = DBCPDataSource.preparedStatement(sql);
+                preparedStatement.setBigDecimal(1, BigDecimal.valueOf(price));
+                preparedStatement.setDate(2, java.sql.Date.valueOf(java.time.LocalDate.now()));
+                preparedStatement.setDouble(3, price); // Thay thế price bằng weight nếu weight là giá trị cần lưu
+                preparedStatement.setInt(4, id);
+
+                synchronized (preparedStatement) {
+                    updated = preparedStatement.executeUpdate();
+                }
+
+                preparedStatement.close();
+
+                if (updated == 1) {
+                    return id;
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        return 0;
+    }
+
+    public static int getNextId() {
+        int nextId = 0;
+        try {
+            PreparedStatement preparedStatement = DBCPDataSource.preparedStatement("SELECT MAX(id) FROM shipping_info");
+            synchronized (preparedStatement) {
+                ResultSet resultSet = preparedStatement.executeQuery();
+                if (resultSet.next()) {
+                    nextId = resultSet.getInt(1) + 1;
+                }
+                resultSet.close();
+            }
+            preparedStatement.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return nextId;
+    }
 }
--- src/main/java/com/example/finallaptrinhweb/model/ShippingInfo.java
+++ src/main/java/com/example/finallaptrinhweb/model/ShippingInfo.java
@@ -1,4 +1,3 @@
-
 package com.example.finallaptrinhweb.model;
 
 import java.math.BigDecimal;
@@ -6,30 +5,18 @@ import java.sql.Date;
 
 public class ShippingInfo {
     private int id;
-    private int orderId;
-    private String trackingNumber;
-    private String shippingMethod;
     private BigDecimal shippingCost;
-    private String deliveryAddress;
-    private Date expectedDeliveryDate;
-    private Date shippedDate;
-    private Date receivedDate;
-    private String status;
+    private Date dateCreated;
+    private double weight;
 
     public ShippingInfo() {
     }
 
-    public ShippingInfo(int id, int orderId, String trackingNumber, String shippingMethod, BigDecimal shippingCost, String deliveryAddress, Date expectedDeliveryDate, Date shippedDate, Date receivedDate, String status) {
+    public ShippingInfo(int id, BigDecimal shippingCost, Date dateCreated, double weight) {
         this.id = id;
-        this.orderId = orderId;
-        this.trackingNumber = trackingNumber;
-        this.shippingMethod = shippingMethod;
         this.shippingCost = shippingCost;
-        this.deliveryAddress = deliveryAddress;
-        this.expectedDeliveryDate = expectedDeliveryDate;
-        this.shippedDate = shippedDate;
-        this.receivedDate = receivedDate;
-        this.status = status;
+        this.dateCreated = dateCreated;
+        this.weight = weight;
     }
 
     public int getId() {
@@ -40,30 +27,6 @@ public class ShippingInfo {
         this.id = id;
     }
 
-    public int getOrderId() {
-        return this.orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getTrackingNumber() {
-        return this.trackingNumber;
-    }
-
-    public void setTrackingNumber(String trackingNumber) {
-        this.trackingNumber = trackingNumber;
-    }
-
-    public String getShippingMethod() {
-        return this.shippingMethod;
-    }
-
-    public void setShippingMethod(String shippingMethod) {
-        this.shippingMethod = shippingMethod;
-    }
-
     public BigDecimal getShippingCost() {
         return this.shippingCost;
     }
@@ -72,47 +35,24 @@ public class ShippingInfo {
         this.shippingCost = shippingCost;
     }
 
-    public String getDeliveryAddress() {
-        return this.deliveryAddress;
+    public Date getDateCreated() {
+        return this.dateCreated;
     }
 
-    public void setDeliveryAddress(String deliveryAddress) {
-        this.deliveryAddress = deliveryAddress;
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
     }
 
-    public Date getExpectedDeliveryDate() {
-        return this.expectedDeliveryDate;
+    public double getWeight() {
+        return this.weight;
     }
 
-    public void setExpectedDeliveryDate(Date expectedDeliveryDate) {
-        this.expectedDeliveryDate = expectedDeliveryDate;
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 
-    public Date getShippedDate() {
-        return this.shippedDate;
-    }
-
-    public void setShippedDate(Date shippedDate) {
-        this.shippedDate = shippedDate;
-    }
-
-    public Date getReceivedDate() {
-        return this.receivedDate;
-    }
-
-    public void setReceivedDate(Date receivedDate) {
-        this.receivedDate = receivedDate;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
+    @Override
     public String toString() {
-        return "ShippingInfo{id=" + this.id + ", orderId=" + this.orderId + ", trackingNumber='" + this.trackingNumber + "', shippingMethod='" + this.shippingMethod + "', shippingCost=" + this.shippingCost + ", deliveryAddress='" + this.deliveryAddress + "', expectedDeliveryDate=" + this.expectedDeliveryDate + ", shippedDate=" + this.shippedDate + ", receivedDate=" + this.receivedDate + ", status='" + this.status + "'}";
+        return "ShippingInfo{id=" + this.id + ", shippingCost=" + this.shippingCost + ", dateCreated=" + this.dateCreated + ", weight=" + this.weight + "}";
     }
 }
